5c09b2a216df9e77ec1bfdd86d371c3b2e77c514
yashksingh200-tech <yashksingh200@gmail.com>
2026-08-06 23:03:18 +0530

    build: remove redundant gradlew script

--- android/app/src/main/java/com/ferioheart/app/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.ferioheart.app;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.ferioheart.app',
-  appName: 'Ferio Heart AI',
-  webDir: 'dist'
-};
-
-export default config;
